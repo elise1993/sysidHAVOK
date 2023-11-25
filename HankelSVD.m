d39571d67119f79e262320be5379dd6a2b7b03a3
Elise Jonsson <elise.jonsson@geo.uu.se>
2023-11-25 11:42:52 +0100

    Implemented support for gpuArrays (only used when Hankel matrix is very large)

--- HankelSVD.m
+++ HankelSVD.m
@@ -9,10 +9,19 @@
 
 function [H,U,S,V] = HankelSVD(x,stackmax)
 
+% construct Hankel matrix
 n = length(x);
 ij = (1:n-stackmax) + (0:(stackmax-1))';
 H = x(ij);
 
+% assign H to GPU if large enough
+if numel(ij) > 1e6 | canUseGPU
+    H = gpuArray(H);
+end
+
+% take singular value decomposition
 [U,S,V] = svd(H,'econ');
+
+[H,U,S,V] = gather(H,U,S,V);
 
 end
